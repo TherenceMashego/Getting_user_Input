e7ba8bc8465dd9f4ae7dc4d6a2d689677474a389
TERRENCE  MASHEGO <TERRENCE  MASHEGO@USERDELLFLOOR2>
2023-12-05 16:00:45 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Getting_user_Input
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Prompt user for name input
+            Console.WriteLine("Enter your name:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hi " + name);
+
+            // Prompt user for age input
+            Console.Write("How old are you? ");
+            // The values from the user are always strings
+            string age = Console.ReadLine();
+            Console.WriteLine("You are " + age + " years old.");
+
+            Console.ReadLine();
+        }
+    }
+}
